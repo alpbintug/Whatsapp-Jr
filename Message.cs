898f8a432e9147cd0ffb3cd55b9ab239f87659eb
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-08-21 09:56:06 +0300

    Create Message.cs

---
+++ Message.cs
@@ -0,0 +1,27 @@
+namespace $safeprojectname$
+{
+    class Message
+    {
+        private string text;
+        private string sender;
+        private string reciever;
+        private string time;
+
+        public string Text { get { return text; } set { text = value; } }
+        public string Sender { get { return sender; } set { sender = value; } }
+        public string Reciever { get { return reciever; } set { reciever = value; } }
+        public string Time { get { return time; } set { time = value; } }
+
+        public Message(string sender, string reciever, string text, string time)
+        {
+            this.sender = sender;
+            this.reciever = reciever;
+            this.text = text;
+            this.time = time;
+        }
+        public Message()
+        {
+
+        }
+    }
+}
