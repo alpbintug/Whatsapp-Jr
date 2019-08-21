6f121d25330032a99e83762668fbc73aa79ab072
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-08-21 09:52:52 +0300

    Create LoginPage.Designer.cs

---
+++ LoginPage.Designer.cs
@@ -0,0 +1,278 @@
+namespace $safeprojectname$
+{
+    partial class LoginPage
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnSignUp = new System.Windows.Forms.Button();
+            this.txtAddRole = new System.Windows.Forms.TextBox();
+            this.txtAddName = new System.Windows.Forms.TextBox();
+            this.txtAddPassword = new System.Windows.Forms.TextBox();
+            this.txtAddUserName = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Role";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(6, 22);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(141, 22);
+            this.txtUserName.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 2);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "User Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 47);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(6, 67);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(141, 22);
+            this.txtPassword.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 3);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "User Name";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(3, 48);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 17);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Password";
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.BackColor = System.Drawing.Color.DarkBlue;
+            this.btnLogIn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnLogIn.Location = new System.Drawing.Point(6, 100);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(141, 36);
+            this.btnLogIn.TabIndex = 16;
+            this.btnLogIn.Text = "Log In";
+            this.btnLogIn.UseVisualStyleBackColor = false;
+            this.btnLogIn.Click += new System.EventHandler(this.BtnLogIn_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnQuit.Location = new System.Drawing.Point(8, 278);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(161, 40);
+            this.btnQuit.TabIndex = 17;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = false;
+            this.btnQuit.Click += new System.EventHandler(this.BtnQuit_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(8, 11);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(161, 261);
+            this.tabControl1.TabIndex = 18;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.tabPage1.Controls.Add(this.txtUserName);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.btnLogIn);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.txtPassword);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(153, 232);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Sign In";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.RoyalBlue;
+            this.tabPage2.Controls.Add(this.btnSignUp);
+            this.tabPage2.Controls.Add(this.txtAddRole);
+            this.tabPage2.Controls.Add(this.txtAddName);
+            this.tabPage2.Controls.Add(this.txtAddPassword);
+            this.tabPage2.Controls.Add(this.txtAddUserName);
+            this.tabPage2.Controls.Add(this.label8);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(153, 232);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Sign Up";
+            // 
+            // btnSignUp
+            // 
+            this.btnSignUp.BackColor = System.Drawing.Color.DarkBlue;
+            this.btnSignUp.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnSignUp.Location = new System.Drawing.Point(7, 188);
+            this.btnSignUp.Name = "btnSignUp";
+            this.btnSignUp.Size = new System.Drawing.Size(140, 38);
+            this.btnSignUp.TabIndex = 19;
+            this.btnSignUp.Text = "Sign Up";
+            this.btnSignUp.UseVisualStyleBackColor = false;
+            this.btnSignUp.Click += new System.EventHandler(this.BtnSignUp_Click);
+            // 
+            // txtAddRole
+            // 
+            this.txtAddRole.Location = new System.Drawing.Point(6, 160);
+            this.txtAddRole.Name = "txtAddRole";
+            this.txtAddRole.Size = new System.Drawing.Size(141, 22);
+            this.txtAddRole.TabIndex = 18;
+            // 
+            // txtAddName
+            // 
+            this.txtAddName.Location = new System.Drawing.Point(6, 115);
+            this.txtAddName.Name = "txtAddName";
+            this.txtAddName.Size = new System.Drawing.Size(141, 22);
+            this.txtAddName.TabIndex = 17;
+            // 
+            // txtAddPassword
+            // 
+            this.txtAddPassword.Location = new System.Drawing.Point(6, 70);
+            this.txtAddPassword.Name = "txtAddPassword";
+            this.txtAddPassword.PasswordChar = '*';
+            this.txtAddPassword.Size = new System.Drawing.Size(141, 22);
+            this.txtAddPassword.TabIndex = 16;
+            // 
+            // txtAddUserName
+            // 
+            this.txtAddUserName.Location = new System.Drawing.Point(6, 23);
+            this.txtAddUserName.Name = "txtAddUserName";
+            this.txtAddUserName.Size = new System.Drawing.Size(141, 22);
+            this.txtAddUserName.TabIndex = 15;
+            // 
+            // LoginPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(181, 328);
+            this.ControlBox = false;
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.btnQuit);
+            this.Name = "LoginPage";
+            this.Text = "Whatsapp Jr.";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.LoginPage_FormClosing);
+            this.Load += new System.EventHandler(this.LoginPage_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox txtUserName;
+        public System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        public System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        public System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Button btnSignUp;
+        private System.Windows.Forms.TextBox txtAddRole;
+        private System.Windows.Forms.TextBox txtAddName;
+        private System.Windows.Forms.TextBox txtAddPassword;
+        private System.Windows.Forms.TextBox txtAddUserName;
+    }
+}
+
