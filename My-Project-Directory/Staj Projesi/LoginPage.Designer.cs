94258c3f61831fd53020c6d64456d467e4ebbfed
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-08-28 15:36:49 +0300

    Update LoginPage.Designer.cs

--- My-Project-Directory/Staj Projesi/LoginPage.Designer.cs
+++ My-Project-Directory/Staj Projesi/LoginPage.Designer.cs
@@ -30,7 +30,6 @@ namespace Staj_Projesi
         {
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.txtUserName = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
@@ -44,7 +43,6 @@ namespace Staj_Projesi
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.btnSignUp = new System.Windows.Forms.Button();
-            this.txtAddRole = new System.Windows.Forms.TextBox();
             this.txtAddName = new System.Windows.Forms.TextBox();
             this.txtAddPassword = new System.Windows.Forms.TextBox();
             this.txtAddUserName = new System.Windows.Forms.TextBox();
@@ -61,15 +59,6 @@ namespace Staj_Projesi
             this.label1.Size = new System.Drawing.Size(45, 17);
             this.label1.TabIndex = 2;
             this.label1.Text = "Name";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 140);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(37, 17);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Role";
             // 
             // txtUserName
             // 
@@ -137,7 +126,7 @@ namespace Staj_Projesi
             // btnQuit
             // 
             this.btnQuit.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.btnQuit.Location = new System.Drawing.Point(8, 278);
+            this.btnQuit.Location = new System.Drawing.Point(8, 235);
             this.btnQuit.Name = "btnQuit";
             this.btnQuit.Size = new System.Drawing.Size(161, 40);
             this.btnQuit.TabIndex = 17;
@@ -152,7 +141,7 @@ namespace Staj_Projesi
             this.tabControl1.Location = new System.Drawing.Point(8, 11);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(161, 261);
+            this.tabControl1.Size = new System.Drawing.Size(161, 218);
             this.tabControl1.TabIndex = 18;
             this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.TabControl1_SelectedIndexChanged);
             // 
@@ -167,7 +156,7 @@ namespace Staj_Projesi
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(153, 232);
+            this.tabPage1.Size = new System.Drawing.Size(153, 189);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Sign In";
             // 
@@ -175,18 +164,16 @@ namespace Staj_Projesi
             // 
             this.tabPage2.BackColor = System.Drawing.Color.RoyalBlue;
             this.tabPage2.Controls.Add(this.btnSignUp);
-            this.tabPage2.Controls.Add(this.txtAddRole);
             this.tabPage2.Controls.Add(this.txtAddName);
             this.tabPage2.Controls.Add(this.txtAddPassword);
             this.tabPage2.Controls.Add(this.txtAddUserName);
             this.tabPage2.Controls.Add(this.label8);
             this.tabPage2.Controls.Add(this.label1);
-            this.tabPage2.Controls.Add(this.label2);
             this.tabPage2.Controls.Add(this.label7);
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(153, 232);
+            this.tabPage2.Size = new System.Drawing.Size(153, 189);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Sign Up";
             // 
@@ -194,20 +181,13 @@ namespace Staj_Projesi
             // 
             this.btnSignUp.BackColor = System.Drawing.Color.DarkBlue;
             this.btnSignUp.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.btnSignUp.Location = new System.Drawing.Point(7, 188);
+            this.btnSignUp.Location = new System.Drawing.Point(6, 143);
             this.btnSignUp.Name = "btnSignUp";
             this.btnSignUp.Size = new System.Drawing.Size(140, 38);
             this.btnSignUp.TabIndex = 19;
             this.btnSignUp.Text = "Sign Up";
             this.btnSignUp.UseVisualStyleBackColor = false;
             this.btnSignUp.Click += new System.EventHandler(this.BtnSignUp_Click);
-            // 
-            // txtAddRole
-            // 
-            this.txtAddRole.Location = new System.Drawing.Point(6, 160);
-            this.txtAddRole.Name = "txtAddRole";
-            this.txtAddRole.Size = new System.Drawing.Size(141, 22);
-            this.txtAddRole.TabIndex = 18;
             // 
             // txtAddName
             // 
@@ -236,7 +216,7 @@ namespace Staj_Projesi
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.ClientSize = new System.Drawing.Size(181, 328);
+            this.ClientSize = new System.Drawing.Size(181, 278);
             this.ControlBox = false;
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.btnQuit);
@@ -258,7 +238,6 @@ namespace Staj_Projesi
         public System.Windows.Forms.TextBox txtUserName;
         public System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
@@ -270,7 +249,6 @@ namespace Staj_Projesi
         private System.Windows.Forms.TabPage tabPage2;
         public System.Windows.Forms.Button btnQuit;
         private System.Windows.Forms.Button btnSignUp;
-        private System.Windows.Forms.TextBox txtAddRole;
         private System.Windows.Forms.TextBox txtAddName;
         private System.Windows.Forms.TextBox txtAddPassword;
         private System.Windows.Forms.TextBox txtAddUserName;
