09a1d736c8a3d587005a4013c73e7b35348c794f
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-08-28 08:40:04 +0300

    Update ChatPage.cs

--- ChatPage.cs
+++ ChatPage.cs
@@ -4,6 +4,7 @@ using FireSharp.Response;
 using System;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tulpep.NotificationWindow;
 
 namespace Staj_Projesi
 {
@@ -38,7 +39,7 @@ namespace Staj_Projesi
             {
                 timerToRefresh.Stop();
                 string time = DateTime.Now.ToString();
-                Message message = new Message(CurrentUser.UserName, TargetUser.UserName, txtMessage.Text, time);
+                Message message = new Message(CurrentUser.UserName, TargetUser.UserName, txtMessage.Text, time, false);
                 System.Console.WriteLine(txtMessage.Text);
                 CurrentUser.Messages.Add(message);
                 TargetUser.Messages.Add(message);
@@ -48,6 +49,9 @@ namespace Staj_Projesi
                 await SaveCurrentUser();
                 await SaveTargetUser();
                 timerToRefresh.Start();
+                txtMessage.Enabled = false;
+                System.Threading.Thread.Sleep(1000);
+                txtMessage.Enabled = true;
             }
         }
         public async Task SaveCurrentUser()
@@ -62,33 +66,56 @@ namespace Staj_Projesi
         }
         private void ChatPage_Load(object sender, System.EventArgs e)
         {
+            this.MinimumSize = this.Size;
             client = new FireSharp.FirebaseClient(config);
+
         }
 
 
         private async void TimerToRefresh_Tick(object sender, System.EventArgs e)
         {
             FirebaseResponse response = await client.GetTaskAsync("User/" + CurrentUser.UserName);
-            if (CurrentUser.Messages.Count != response.ResultAs<User>().Messages.Count)
+            User responseAsUser = response.ResultAs<User>();
+            if (CurrentUser.Messages.Count < responseAsUser.Messages.Count)
             {
-                CurrentUser.Messages = response.ResultAs<User>().Messages;
-                FirebaseResponse response1 = await client.GetTaskAsync("User/" + TargetUser.UserName);
-                if (TargetUser != response1.ResultAs<User>())
-
-                    //lstChat.Items.Clear();
-
-                    foreach (var item in CurrentUser.Messages)
+                var popupNotifier = new PopupNotifier();
+                if (responseAsUser.Messages.Count - CurrentUser.Messages.Count > 1)
+                    popupNotifier.TitleText = "You have " + (responseAsUser.Messages.Count - CurrentUser.Messages.Count) + " new messages.";
+                else
+                    popupNotifier.TitleText = "You have one new message.";
+                string content = null;
+                for (int i = 0; i < CurrentUser.Messages.Count; i++)
+                {
+                    if (CurrentUser.Messages[i].Sender != CurrentUser.UserName && i > mainPage.lastNotificationMessageIndex)
                     {
-                        if (item.Sender == CurrentUser.UserName && item.Reciever == TargetUser.UserName && !lstChat.Items.Contains("You: " + item.Text))
-                        {
-                            lstChat.Items.Add(item.Time + " - You: " + item.Text);
-                        }
-                        else if (item.Sender == TargetUser.UserName && item.Reciever == CurrentUser.UserName && !lstChat.Items.Contains(item.Sender + ": " + item.Text))
-                        {
-                            lstChat.Items.Add(item.Time + " - " + item.Sender + ": " + item.Text);
-                        }
+                        mainPage.lastNotificationMessageIndex = i;//BURAYI DEĞİŞTİR BURASI SADECE TEK BİR CONTACT İÇİN ÇALIŞIR!!!
+                        content += "-" + CurrentUser.Messages[i].Sender + ": " + CurrentUser.Messages[i].Text + "\n";
+                        CurrentUser.Messages[i].IsSeen = true;
+                    }
+                }
+                popupNotifier.ContentText = content;
+                popupNotifier.IsRightToLeft = false;
+                popupNotifier.BodyColor = System.Drawing.Color.LightBlue;
+                popupNotifier.Popup();
+                for (int i = CurrentUser.Messages.Count; i < responseAsUser.Messages.Count; i++)
+                {
+                    CurrentUser.Messages.Add(responseAsUser.Messages[i]);
+                }
+                lstChat.Items.Clear();
+
+                foreach (var item in CurrentUser.Messages)
+                {
+                    if (item.Sender == CurrentUser.UserName && item.Reciever == TargetUser.UserName && !lstChat.Items.Contains("You: " + item.Text))
+                    {
+                        lstChat.Items.Add(item.Time + " - You: " + item.Text);
+                    }
+                    else if (item.Sender == TargetUser.UserName && item.Reciever == CurrentUser.UserName && !lstChat.Items.Contains(item.Sender + ": " + item.Text))
+                    {
+                        lstChat.Items.Add(item.Time + " - " + item.Sender + ": " + item.Text);
+                        item.IsSeen = true;
 
                     }
+                }
             }
         }
 
@@ -109,16 +136,19 @@ namespace Staj_Projesi
                 {
                     timerToRefresh.Stop();
 
-                    Message message = new Message(CurrentUser.UserName, TargetUser.UserName, txtMessage.Text, DateTime.Now.ToString());
+                    Message message = new Message(CurrentUser.UserName, TargetUser.UserName, txtMessage.Text, DateTime.Now.ToString(), false);
                     System.Console.WriteLine(txtMessage.Text);
                     CurrentUser.Messages.Add(message);
                     TargetUser.Messages.Add(message);
                     string msgToAdd = message.Time + " - You: " + message.Text;
                     lstChat.Items.Add(msgToAdd);
                     txtMessage.Text = "";
+                    //txtMessage.Enabled = false;
                     await SaveCurrentUser();
                     await SaveTargetUser();
                     timerToRefresh.Start();
+                    System.Threading.Thread.Sleep(1000);
+                    //txtMessage.Enabled = true;
                 }
             }
         }
